de3bba9ae8bb7734970f7718fc98729d04ed5e55
RaquelGarciaCaja <raquelgc8@gmail.com>
2021-01-13 00:35:26 +0100

    add no write into input

--- src/js/main.js
+++ src/js/main.js
@@ -5,46 +5,60 @@ const addButton = document.querySelector(".js-btnAdd");
 const titleContacts = document.querySelector(".js-title");
 
 let allContacts = [];
-let addContact = true;
 
 const newContact = () => {
   const inputName = document.querySelector(".js-inputName");
   const inputEmail = document.querySelector(".js-inputEmail");
-  let nameNameValue = inputName.value;
-  let nameEmailValue = inputEmail.value;
-  allContacts.push(nameNameValue + nameEmailValue);
+  let inputNameValue = inputName.value;
+  let inputEmailValue = inputEmail.value;
+  allContacts.push(inputNameValue + inputEmailValue);
   initAddParticipantListener();
 };
 
 function initAddParticipantListener() {
   const list = document.querySelector(".js-list");
+  const inputName = document.querySelector(".js-inputName");
+  const inputEmail = document.querySelector(".js-inputEmail");
+  let inputNameValue = inputName.value;
+  let inputEmailValue = inputEmail.value;
 
   list.innerHTML = "";
   for (let i = 0; i < allContacts.length; i++) {
     const newTr = document.createElement("tr");
     const newTd = document.createElement("td");
+    const newP = document.createElement("p");
     const newButton = document.createElement("button");
 
+    const textInputs = document.createTextNode("Rellena los campos requeridos");
     const textButton = document.createTextNode("X");
     const newContact = document.createTextNode(allContacts[i]);
 
     newTr.appendChild(newTd);
+    newTr.appendChild(newTd);
+
     newTr.appendChild(newButton);
     newTd.appendChild(newContact);
     newTd.appendChild(newContact);
     newButton.appendChild(textButton);
+    newP.appendChild(textInputs);
 
     newTr.classList.add("main__name-email");
     newTd.classList.add("main__input-name");
     newButton.classList.add("main__delete");
-    if (addContact == true) {
-      newButton.onclick = function (ev) {
-        deleteEachItem(ev);
-      };
+    newP.classList.add("main__requiered");
+
+    newButton.onclick = function (ev) {
+      deleteEachItem(ev);
+    };
+
+    if (inputNameValue.length == 0 || inputEmailValue.length == 0) {
+      console.log("no escrito");
+      list.appendChild(newP);
+      // location.reload();
+    } else {
+      console.log("escrito");
       newButton.id = allContacts[i];
       list.appendChild(newTr);
-    } else if (addContact == false) {
-      list.appendChild("");
     }
   }
   document.querySelector(".js-inputName").value = "";
@@ -64,5 +78,16 @@ function listenTrashItem() {
     deleteItem.addEventListener("click", deleteEachItem);
   }
 }
+
+// function check() {
+//   const inputName = document.querySelector(".js-inputName");
+//   const inputEmail = document.querySelector(".js-inputEmail");
+//   let inputNameValue = inputName.value;
+//   let inputEmailValue = inputEmail.value;
+
+//   if (inputNameValue.length == 0 || inputEmailValue.length == 0) {
+//     return false;
+//   }
+// }
 
 addButton.addEventListener("click", newContact);
