1a575aa9c5259aacf763c03e29ae3520bd4184ad
RaquelGarciaCaja <raquelgc8@gmail.com>
2021-01-12 23:53:55 +0100

    add add and remove contacts

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,52 @@
 "use strict";
 
-window.addEventListener("load", init, false);
-
 const list = document.querySelector(".main__list");
 const addButton = document.querySelector(".js-btnAdd");
 const titleContacts = document.querySelector(".js-title");
 
 let allContacts = [];
+let addContact = true;
 
-function init() {
-  initAddParticipantListener();
-}
-
-function initAddParticipantListener() {
-  const list = document.querySelector(".js-list");
+const newContact = () => {
   const inputName = document.querySelector(".js-inputName");
   const inputEmail = document.querySelector(".js-inputEmail");
-  const newButton = document.querySelector(".js-btnAdd");
-
   let nameNameValue = inputName.value;
   let nameEmailValue = inputEmail.value;
   allContacts.push(nameNameValue + nameEmailValue);
+  initAddParticipantListener();
+};
+
+function initAddParticipantListener() {
+  const list = document.querySelector(".js-list");
 
   list.innerHTML = "";
   for (let i = 0; i < allContacts.length; i++) {
     const newTr = document.createElement("tr");
     const newTd = document.createElement("td");
     const newButton = document.createElement("button");
+
+    const textButton = document.createTextNode("X");
     const newContact = document.createTextNode(allContacts[i]);
 
     newTr.appendChild(newTd);
     newTr.appendChild(newButton);
     newTd.appendChild(newContact);
     newTd.appendChild(newContact);
+    newButton.appendChild(textButton);
 
     newTr.classList.add("main__name-email");
     newTd.classList.add("main__input-name");
     newButton.classList.add("main__delete");
-
-    newButton.onclick = function (ev) {
-      deleteEachItem(ev);
-    };
-    newButton.id = allContacts[i];
-    list.appendChild(newTr);
+    if (addContact == true) {
+      newButton.onclick = function (ev) {
+        deleteEachItem(ev);
+      };
+      newButton.id = allContacts[i];
+      list.appendChild(newTr);
+    } else if (addContact == false) {
+      list.appendChild("");
+    }
   }
-
   document.querySelector(".js-inputName").value = "";
   document.querySelector(".js-inputEmail").value = "";
   document.querySelector(".js-inputName").focus();
@@ -57,10 +59,10 @@ function deleteEachItem(ev) {
   initAddParticipantListener();
 }
 function listenTrashItem() {
-  const deleteItems = document.querySelectorAll(".js-delete");
+  const deleteItems = document.querySelectorAll(".main__delete");
   for (const deleteItem of deleteItems) {
     deleteItem.addEventListener("click", deleteEachItem);
   }
 }
 
-addButton.addEventListener("click", initAddParticipantListener);
+addButton.addEventListener("click", newContact);
