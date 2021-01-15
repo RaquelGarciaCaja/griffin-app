b4f3f9c2ca2b146faa9559f736986666c42b0534
RaquelGarciaCaja <raquelgc8@gmail.com>
2021-01-15 15:36:07 +0100

    add new object and array

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 
 const list = document.querySelector(".main__list");
 const addButton = document.querySelector(".js-btnAdd");
-const titleContacts = document.querySelector(".js-title");
+const titleContacts = document.querySelector(".main__title");
 
 let allContacts = [];
 
@@ -11,55 +11,61 @@ const newContact = () => {
   const inputEmail = document.querySelector(".js-inputEmail");
   let inputNameValue = inputName.value;
   let inputEmailValue = inputEmail.value;
-  allContacts.push(inputNameValue + inputEmailValue);
-  initAddParticipantListener();
+
+  function Contact(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+  let newObjContacts = new Contact(inputNameValue, inputEmailValue);
+  initAddParticipantListener(newObjContacts);
 };
 
-function initAddParticipantListener() {
+function initAddParticipantListener(newObjContacts) {
+  // const inputName = document.querySelector(".js-inputName");
+  // const inputEmail = document.querySelector(".js-inputEmail");
+  // let inputNameValue = inputName.value;
+  // let inputEmailValue = inputEmail.value;
   const list = document.querySelector(".js-list");
-  const inputName = document.querySelector(".js-inputName");
-  const inputEmail = document.querySelector(".js-inputEmail");
-  let inputNameValue = inputName.value;
-  let inputEmailValue = inputEmail.value;
+  allContacts.push(newObjContacts);
 
   list.innerHTML = "";
   for (let i = 0; i < allContacts.length; i++) {
-    const newTr = document.createElement("tr");
-    const newTd = document.createElement("td");
-    const newP = document.createElement("p");
+    const newTr = document.createElement("tbody");
+    const newTdName = document.createElement("td");
+    const newTdEmail = document.createElement("td");
     const newButton = document.createElement("button");
 
-    const textInputs = document.createTextNode("Rellena los campos requeridos");
     const textButton = document.createTextNode("X");
-    const newContact = document.createTextNode(allContacts[i]);
+    console.log(allContacts[i]);
+    const newContactName = document.createTextNode(allContacts[i].name);
+    const newContactEmail = document.createTextNode(allContacts[i].email);
 
-    newTr.appendChild(newTd);
-    newTr.appendChild(newTd);
-
+    newTr.appendChild(newTdName);
+    newTr.appendChild(newTdEmail);
     newTr.appendChild(newButton);
-    newTd.appendChild(newContact);
-    newTd.appendChild(newContact);
+    newTdName.appendChild(newContactName);
+    newTdEmail.appendChild(newContactEmail);
     newButton.appendChild(textButton);
-    newP.appendChild(textInputs);
 
     newTr.classList.add("main__name-email");
-    newTd.classList.add("main__input-name");
+    newTdName.classList.add("main__input-name");
+    newTdEmail.classList.add("main__input-name");
     newButton.classList.add("main__delete");
-    newP.classList.add("main__requiered");
 
     newButton.onclick = function (ev) {
       deleteEachItem(ev);
     };
 
-    if (inputNameValue.length == 0 || inputEmailValue.length == 0) {
-      console.log("no escrito");
-      list.appendChild(newP);
-      // location.reload();
-    } else {
-      console.log("escrito");
-      newButton.id = allContacts[i];
-      list.appendChild(newTr);
-    }
+    newButton.id = allContacts[i];
+    list.appendChild(newTr);
+
+    // if (inputNameValue.length == 0 || inputEmailValue.length == 0) {
+    //   // list.appendChild(newP);
+    //   preventDefault();
+    // } else {
+    //   newButton.id = allContacts[i];
+    //   list.appendChild(newTr);
+    // }
   }
   document.querySelector(".js-inputName").value = "";
   document.querySelector(".js-inputEmail").value = "";
@@ -72,22 +78,12 @@ function deleteEachItem(ev) {
   allContacts.splice(index, 1);
   initAddParticipantListener();
 }
+
 function listenTrashItem() {
   const deleteItems = document.querySelectorAll(".main__delete");
   for (const deleteItem of deleteItems) {
     deleteItem.addEventListener("click", deleteEachItem);
   }
 }
-
-// function check() {
-//   const inputName = document.querySelector(".js-inputName");
-//   const inputEmail = document.querySelector(".js-inputEmail");
-//   let inputNameValue = inputName.value;
-//   let inputEmailValue = inputEmail.value;
-
-//   if (inputNameValue.length == 0 || inputEmailValue.length == 0) {
-//     return false;
-//   }
-// }
 
 addButton.addEventListener("click", newContact);
